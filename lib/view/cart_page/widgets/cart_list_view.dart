4685aff15d605ed2f5e6bb7381305456760d6094
jahid hasan <jh@mortgage-magic.co.uk>
2024-01-03 13:09:22 +0600

    No product Found Added

--- lib/view/cart_page/widgets/cart_list_view.dart
+++ lib/view/cart_page/widgets/cart_list_view.dart
@@ -81,128 +81,132 @@ class _CartListViewState extends State<CartListView> {
           valueListenable: Boxes.getData().listenable(),
           builder: (context, box, _) {
             final data = box.values.toList().cast<Product>();
-            return ListView.builder(
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemBuilder: (c, i) => Padding(
-                padding: const EdgeInsets.only(bottom: 10, top: 10),
-                child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: const Color(0xffF2F2F4)),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              height: 60,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12),
-                                  color: Colors.grey.shade100),
-                              child: Image.network(
-                                data[i].images.first.toString(),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Expanded(
-                            child: Column(
+            return data.isEmpty
+                ? const Center(child: Text('No Product Found'))
+                : ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemBuilder: (c, i) => Padding(
+                      padding: const EdgeInsets.only(bottom: 10, top: 10),
+                      child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: const Color(0xffF2F2F4)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text(
-                                  data[i].title,
-                                  style: const TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
+                                Expanded(
+                                  flex: 2,
+                                  child: Container(
+                                    height: 60,
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(12),
+                                        color: Colors.grey.shade100),
+                                    child: Image.network(
+                                      data[i].images.first.toString(),
+                                    ),
+                                  ),
                                 ),
-                                const Text('Size L',
-                                    style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w300)),
-                                Text('\$ ${data[i].price}',
-                                    style: const TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold)),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        data[i].title,
+                                        style: const TextStyle(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      const Text('Size L',
+                                          style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.w300)),
+                                      Text('\$ ${data[i].price}',
+                                          style: const TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold)),
+                                    ],
+                                  ),
+                                ),
+                                Expanded(
+                                    child: Column(
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        // box.deleteAt(i);
+                                        _calculateTotalPrice(
+                                            excludedProductId: i);
+                                        _controllerPd.voucherDiscount.value = 0;
+                                        _controllerPd
+                                            .calculateSubAndQantityTotalPrice();
+                                      },
+                                      child: const Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Row(
+                                      children: [
+                                        InkWell(
+                                          onTap: () {
+                                            _incrementCounter(i, data[i].price);
+                                          },
+                                          child: Container(
+                                              decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      color: Colors.black),
+                                                  borderRadius:
+                                                      BorderRadius.circular(8)),
+                                              child: const Icon(
+                                                Icons.add,
+                                                size: 15,
+                                              )),
+                                        ),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          '${data[i].quantity}',
+                                          style: const TextStyle(fontSize: 15),
+                                        ),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        InkWell(
+                                          onTap: () {
+                                            _dcrementCounter(i, data[i].price);
+                                          },
+                                          child: Container(
+                                              decoration: BoxDecoration(
+                                                  border: Border.all(
+                                                      color: Colors.black),
+                                                  borderRadius:
+                                                      BorderRadius.circular(8)),
+                                              child: const Icon(
+                                                Icons.remove,
+                                                size: 15,
+                                              )),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ))
                               ],
                             ),
-                          ),
-                          Expanded(
-                              child: Column(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  // box.deleteAt(i);
-                                  _calculateTotalPrice(excludedProductId: i);
-                                  _controllerPd.voucherDiscount.value = 0;
-                                  _controllerPd
-                                      .calculateSubAndQantityTotalPrice();
-                                },
-                                child: const Icon(
-                                  Icons.delete,
-                                  color: Colors.red,
-                                ),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Row(
-                                children: [
-                                  InkWell(
-                                    onTap: () {
-                                      _incrementCounter(i, data[i].price);
-                                    },
-                                    child: Container(
-                                        decoration: BoxDecoration(
-                                            border:
-                                                Border.all(color: Colors.black),
-                                            borderRadius:
-                                                BorderRadius.circular(8)),
-                                        child: const Icon(
-                                          Icons.add,
-                                          size: 15,
-                                        )),
-                                  ),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    '${data[i].quantity}',
-                                    style: const TextStyle(fontSize: 15),
-                                  ),
-                                  const SizedBox(
-                                    width: 5,
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      _dcrementCounter(i, data[i].price);
-                                    },
-                                    child: Container(
-                                        decoration: BoxDecoration(
-                                            border:
-                                                Border.all(color: Colors.black),
-                                            borderRadius:
-                                                BorderRadius.circular(8)),
-                                        child: const Icon(
-                                          Icons.remove,
-                                          size: 15,
-                                        )),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ))
-                        ],
-                      ),
-                    )),
-              ),
-              itemCount: box.length,
-            );
+                          )),
+                    ),
+                    itemCount: box.length,
+                  );
           }),
     );
   }
